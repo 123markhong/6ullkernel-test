062cea228859fa734f7930ce7eabcb95f4f1ed57
123markhong <2386712909@qq.com>
2022-08-12 10:04:17 +0800

    add 6ull-kernel5.10.9 branch

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #source /opt/imx6ul/environment-setup-cortexa7t2hf-neon-poky-linux-gnueabi
 source /opt/6ul-5.10sdk/environment-setup-cortexa7t2hf-neon-poky-linux-gnueabi
-#make distclean
+make distclean
 
 make ARCH=arm  myd_y6ulx_defconfig
 #make ARCH=arm O="$PWD/../build" mys_6ulx_defconfig
 #make mys_6ulx_defconfig
 make ARCH=arm zImage dtbs   -j16
-make  modules  -j16
+#make  modules  -j16
 
 
 #make modules_install O="$PWD/../build"
